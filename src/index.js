a391257c2b8e0461bfecbef57fb88f21f133b339
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-07-26 17:48:13 +0530

    sdfsF

--- src/index.js
+++ src/index.js
@@ -21,6 +21,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (_req, res) => {
+  res.send('API is running...');
+});
+
 app.post('/api/login', LoginUser);
 app.use('/api/products', productRoutes);
 app.use('/api/blogs', blogRoutes);
